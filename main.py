2e1fca88bafefe3ecea24c5a63509d5c9ff8a3c5
ivootxt <ivanaraolaza@gmail.com>
2023-06-30 11:58:03 -0300

    Bugfix

--- main.py
+++ main.py
@@ -6,28 +6,31 @@ cuit_cuil_prefixes = ["20", "23", "24", "25", "26", "27", "30", "33", "34"]
 
 def fibertel() -> None:
     """
-    Generates Fibertel Home Networks wordlist
+        Generates Fibertel Home Networks wordlist
     """
     print("[+] Creating dictionaries for home networks...")
     fibertel = open("fibertel.txt", "a")
 
     for prefix in home_prefixes:
         for i in range (1000000, 50000000):
-            fibertel.write('{0:03d}{1:08d}'.format(int(prefix), int(i)))
-            fibertel.write('{0:03d}{1:07d}'.format(int(prefix), int(i))) # Certain confution exists about if the DNI is the whole number or the first 7 digits, so I will double the posibilities.
+            fibertel.write('{0:03d}{1:08d}\n'.format(int(prefix), int(i)))
+            fibertel.write('{0:03d}{1:07d}\n'.format(int(prefix), int(i))) # Certain confution exists about if the DNI is the whole number or the first 7 digits, so I will double the posibilities.
     fibertel.close()
     print("Done")
 
 
 def fibercorp() -> None:
+    """
+        Generates Fibertel Corporative Networks wordlist
+    """
     print("[+] Creating dictionaries for corpotative networks...")
     fibercorp = open("fibercorp.txt", "a")
     
     for prefix in corp_prefixes:
         for prefix_ in cuit_cuil_prefixes:
             for i in range (0, 100000):
-                fibercorp.write('{0:03d}{1:2d}{2:05d}'.format(int(prefix), int(prefix_), int(i)))
-                fibercorp.write('{0:03d}{1:2d}{2:09d}'.format(int(prefix), int(prefix_), int(i))) #the same confurion apperares here so I will duplicate again.
+                fibercorp.write('{0:03d}{1:2d}{2:05d}\n'.format(int(prefix), int(prefix_), int(i)))
+                fibercorp.write('{0:03d}{1:2d}{2:09d}\n'.format(int(prefix), int(prefix_), int(i))) #the same confurion apperares here so I will duplicate again.
     fibercorp.close()
     print("Done")
 
